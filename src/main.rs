3acc011ddf9754bb6550e16c341d3764d0d49e69
Richard Bates <rich.bates@protonmail.com>
2023-05-21 03:41:23 +0100

    Process account actions from user input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    io::{self},
+};
 
 /// An application-specific error type
 #[derive(Debug)]
@@ -18,7 +21,7 @@ pub enum Tx {
 }
 
 /// A type for managing accounts and their current currency balance
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Accounts {
     accounts: HashMap<String, u64>,
 }
@@ -109,63 +112,122 @@ impl Accounts {
     }
 }
 
+fn read_from_stdin(label: &str) -> String {
+    println!("{label}");
+
+    let mut user_input = String::new();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Sorry please try again");
+
+    return user_input;
+}
+
+fn process_actions(mut ledger: Accounts, action: &str) {
+    let signer = read_from_stdin("What is the signer account name?");
+    let amount = read_from_stdin("What is the amount?");
+    let amount = amount
+        .trim()
+        .parse::<u64>()
+        .expect("Please input a valid number");
+
+    match action {
+        "deposit" | "DEPOSIT" => {
+            match ledger.deposit(&signer.trim(), amount) {
+                Ok(tx) => {
+                    println!("Depositing {} for {}: {:?}", amount, signer, tx)
+                }
+                Err(e) => eprintln!("Something went wrong: {:?}", e),
+            };
+        }
+        "withdraw" | "WITHDRAW" => {
+            match ledger.withdraw(&signer.trim(), amount) {
+                Ok(tx) => {
+                    println!("Withdrawing {} from {}: {:?}", amount, signer, tx)
+                }
+                Err(e) => eprintln!("Something went wrong: {:?}", e),
+            };
+        }
+        "send" | "SEND" => {
+            let recipient = read_from_stdin("What is the recipient account name?");
+
+            match ledger.send(&signer.trim(), &recipient.trim(), amount) {
+                Ok(tx) => {
+                    println!(
+                        "Sending {} from {} to {}: {:?}",
+                        amount, signer, recipient, tx
+                    )
+                }
+                Err(e) => eprintln!("Something went wrong: {:?}", e),
+            };
+        }
+        &_ => todo!(),
+    }
+}
+
 fn main() {
-    println!("Hello, accounting world!");
+    let ledger = Accounts::new();
+    loop {
+        let input = read_from_stdin("What do you want to do?");
+        let input = input.trim();
 
-    // We are using simple &str instances as keys
-    // for more sophisticated keys (e.g. hashes)
-    // the data type could remain the same
-    let bob = "bob";
-    let alice = "alice";
-    let charlie = "charlie";
-    let initial_amount = 100;
+        if input.is_empty() {
+            println!("Goodbye!");
+            break;
+        }
 
-    // Creates the basic ledger and a tx log container
-    let mut ledger = Accounts::new();
-    let mut tx_log = vec![];
-
-    // Deposit an amount to each account
-    for signer in &[bob, alice, charlie] {
-        let status = ledger.deposit(*signer, initial_amount);
-        println!("Depositing {} for {}: {:?}", signer, initial_amount, status);
-        // Add the resulting transaction to a list of transactions
-        // .unwrap() will crash the program if the status is an error.
-        tx_log.push(status.unwrap());
+        println!("You selected: {}", input);
+        // "Remove the ledger clone later on(use reference instead!!!!!"
+        // Add transaction history to Accounts struct as a vec[]
+        process_actions(ledger.clone(), &input)
     }
 
-    // Send currency from one account (bob) to the other (alice)
-    let send_amount = 10_u64;
-    let status = ledger.send(bob, alice, send_amount);
-    println!(
-        "Sent {} from {} to {}: {:?}",
-        send_amount, bob, alice, status
-    );
+    // // Creates the basic ledger and a tx log container
+    // let mut ledger = Accounts::new();
+    // let mut tx_log = vec![];
 
-    // Add both transactions to the transaction log
-    let (tx1, tx2) = status.unwrap();
-    tx_log.push(tx1);
-    tx_log.push(tx2);
+    // // Deposit an amount to each account
+    // for signer in &[bob, alice, charlie] {
+    //     let status = ledger.deposit(*signer, initial_amount);
+    //     println!("Depositing {} for {}: {:?}", signer, initial_amount, status);
+    //     // Add the resulting transaction to a list of transactions
+    //     // .unwrap() will crash the program if the status is an error.
+    //     tx_log.push(status.unwrap());
+    // }
 
-    // Withdraw everything from the accounts
-    let tx = ledger.withdraw(charlie, initial_amount).unwrap();
-    tx_log.push(tx);
-    let tx = ledger
-        .withdraw(alice, initial_amount + send_amount)
-        .unwrap();
-    tx_log.push(tx);
+    // // Send currency from one account (bob) to the other (alice)
+    // let send_amount = 10_u64;
+    // let status = ledger.send(bob, alice, send_amount);
+    // println!(
+    //     "Sent {} from {} to {}: {:?}",
+    //     send_amount, bob, alice, status
+    // );
 
-    // Here we are withdrawing too much and there won't be a transaction
-    println!(
-        "Withdrawing {} from {}: {:?}",
-        initial_amount,
-        bob,
-        ledger.withdraw(bob, initial_amount)
-    );
-    // Withdrawing the expected amount results in a transaction
-    let tx = ledger.withdraw(bob, initial_amount - send_amount).unwrap();
-    tx_log.push(tx);
+    // // Add both transactions to the transaction log
+    // let (tx1, tx2) = status.unwrap();
+    // tx_log.push(tx1);
+    // tx_log.push(tx2);
 
-    // {:?} prints the Debug implementation, {:#?} pretty-prints it
-    println!("Ledger empty: {:?}", ledger);
-    println!("The TX log: {:#?}", tx_log);
+    // // Withdraw everything from the accounts
+    // let tx = ledger.withdraw(charlie, initial_amount).unwrap();
+    // tx_log.push(tx);
+    // let tx = ledger
+    //     .withdraw(alice, initial_amount + send_amount)
+    //     .unwrap();
+    // tx_log.push(tx);
+
+    // // Here we are withdrawing too much and there won't be a transaction
+    // println!(
+    //     "Withdrawing {} from {}: {:?}",
+    //     initial_amount,
+    //     bob,
+    //     ledger.withdraw(bob, initial_amount)
+    // );
+    // // Withdrawing the expected amount results in a transaction
+    // let tx = ledger.withdraw(bob, initial_amount - send_amount).unwrap();
+    // tx_log.push(tx);
+
+    // // {:?} prints the Debug implementation, {:#?} pretty-prints it
+    // println!("Ledger empty: {:?}", ledger);
+    // println!("The TX log: {:#?}", tx_log);
 }
