a948b9c1a855d3216e1817f0a3d4aa7670743df4
Richard Bates <rich.bates@protonmail.com>
2023-05-27 02:05:04 +0100

    Moves cli content to web binary crate

--- octopus_web/src/main.rs
+++ octopus_web/src/main.rs
@@ -1,3 +1,11 @@
+mod accounting;
+mod core;
+mod trading_platform;
+
+use crate::core::{Order, Side};
+use octopus_common::{errors, tx};
+use trading_platform::TradingPlatform;
+
 fn main() {
     println!("Hello, world!");
 }
