b4c4d46487d1722ab7cc8664b8f50616bc95e685
Richard Bates <rich.bates@protonmail.com>
2023-05-27 02:31:24 +0100

    Setup web binary dependencies

--- octopus_web/src/main.rs
+++ octopus_web/src/main.rs
@@ -2,10 +2,16 @@ mod accounting;
 mod core;
 mod trading_platform;
 
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
 use crate::core::{Order, Side};
 use octopus_common::{errors, tx};
 use trading_platform::TradingPlatform;
 
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init();
+
+    info!("starting up");
 }
