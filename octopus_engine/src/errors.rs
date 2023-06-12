4541a88c5d7fedfd55e9b716b63b9728b013cd32
Richard Bates <rich.bates@protonmail.com>
2023-06-12 02:31:49 +0200

    implements error trait for OctopusError

--- octopus_engine/src/errors.rs
+++ octopus_engine/src/errors.rs
@@ -1,8 +1,23 @@
+use std::{error::Error, fmt};
+
+use serde::{Deserialize, Serialize};
+
 /// An application-specific error type
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum AccountError {
     // Add variants here for account not found, account underfunded and account overfunded
     NotFound(String),
     OverFunded(String, u64),
     UnderFunded(String),
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OctopusError(pub AccountError);
+
+impl fmt::Display for OctopusError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "There is an error: {:?}", self.0)
+    }
+}
+
+impl Error for OctopusError {}
